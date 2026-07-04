0ce910511964bf6944a99032e86e665b0c955af7
lucasfernandescoder <>
2026-07-04 13:04:05 +0000

    Loop Counters

---
+++ the_for_loop/loop_counters/main.py
@@ -0,0 +1,7 @@
+# List of countries
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'China', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+counter = 0
+for country in countries:
+    if len(country) == 6:
+        counter += 1
+print("Number of countries with exactly 6 letters:", counter)
